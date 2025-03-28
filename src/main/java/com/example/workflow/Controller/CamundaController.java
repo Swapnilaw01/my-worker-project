038e7f2ac31ce36d91ce703dfd302d2aa7d6e66f
Swapnil <swapnilaw01@gmail.com>
2025-03-29 00:20:48 +0100

    Added Camunda Java api implementation

--- src/main/java/com/example/workflow/Controller/CamundaController.java
+++ src/main/java/com/example/workflow/Controller/CamundaController.java
@@ -1,8 +1,14 @@
 package com.example.workflow.Controller;
 
 import com.example.workflow.Service.CamundaService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/camunda")
@@ -10,10 +16,23 @@ public class CamundaController {
 
     @Autowired
     private CamundaService camundaService;
+    @Autowired
+    private RuntimeService runtimeService;
 
     @GetMapping("/start")
     public String startProcess(){
         return camundaService.startProcess();
+    }
+
+    @PostMapping("/start/process")
+    public String startProcess(@RequestBody Map<String, Object> variables) {
+        VariableMap variableMap = Variables.createVariables();
+        variableMap.putAll(variables);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("MyWorkerProcess",
+                variableMap
+        );
+
+        return "Process started: " + processInstance.getId();
     }
 
     @GetMapping("/task/complete")
